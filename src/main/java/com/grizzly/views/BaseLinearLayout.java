83ead8ee4fe63c5f4c9751fb86b7de460eb39616
fcopardo <francisco.pardo.baeza@gmail.com>
2016-03-06 03:32:45 -0300

    .

---
+++ src/main/java/com/grizzly/views/BaseLinearLayout.java
@@ -0,0 +1,48 @@
+package com.grizzly.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+/**
+ * Created by FcoPardo on 3/6/16.
+ *
+ * Base class for creating custom views using a LinearLayout.
+ */
+public abstract class BaseLinearLayout extends LinearLayout {
+
+    protected int layout = 0;
+
+    public BaseLinearLayout(Context context) {
+        super(context);
+    }
+
+    public BaseLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public BaseLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public BaseLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    protected void inflateBaseLayout(){
+        setContainer();
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(infService);
+        inflater.inflate(layout, this, true);
+        inflateComponents();
+    }
+
+    protected abstract void inflateComponents();
+
+    protected abstract void setContainer();
+}
---
+++ src/main/java/com/grizzly/views/BaseRelativeLayout.java
@@ -0,0 +1,45 @@
+package com.grizzly.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by FcoPardo on 3/6/16.
+ */
+public abstract class BaseRelativeLayout extends RelativeLayout {
+
+    protected int layout = 0;
+
+    public BaseRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public BaseRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public BaseRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    protected void inflateBaseLayout(){
+        setContainer();
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(infService);
+        inflater.inflate(layout, this, true);
+        inflateComponents();
+    }
+
+    protected abstract void inflateComponents();
+
+    protected abstract void setContainer();
+}
