96e6cb1ba7d4bda127029c40eca569122aa3c80e
Fco Pardo <Francisco.Pardo.Baeza@gmail.com>
2017-05-22 00:35:48 -0400

    added constraintlayout

---
+++ src/main/java/com/grizzly/baseViews/AbstractConstraintLayout.java
@@ -0,0 +1,32 @@
+package com.grizzly.baseViews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by FcoPardo on 3/6/16.
+ */
+public abstract class AbstractConstraintLayout<T> extends BaseConstraintLayout {
+
+    protected T data;
+    protected Class<T> dataClass;
+
+    public AbstractConstraintLayout(Context context) {
+        super(context);
+    }
+
+    public AbstractConstraintLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AbstractConstraintLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setData(T data){
+        this.data = data;
+        setControls();
+    }
+
+    protected abstract void setControls();
+}
---
+++ src/main/java/com/grizzly/baseViews/BaseConstraintLayout.java
@@ -0,0 +1,44 @@
+package com.grizzly.baseViews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by FcoPardo on 3/6/16.
+ */
+public abstract class BaseConstraintLayout extends ConstraintLayout {
+
+    protected int layout = 0;
+
+    public BaseConstraintLayout(Context context) {
+        super(context);
+        inflateBaseLayout();
+    }
+
+    public BaseConstraintLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        inflateBaseLayout();
+    }
+
+    public BaseConstraintLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        inflateBaseLayout();
+    }
+
+
+    protected void inflateBaseLayout(){
+        setContainer();
+        if(layout>0)BaseView.inflateLayout(layout, getContext(), this);
+        inflateComponents();
+    }
+
+    protected abstract void inflateComponents();
+
+    protected abstract void setContainer();
+
+    protected Activity getActivity(){
+        return BaseView.getActivity(this);
+    }
+}
